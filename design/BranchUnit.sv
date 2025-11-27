69664c607643c8b948a8f597e5e1a831448a8232
rvnicolau <rvno@cin.ufpe.br>
2025-11-27 10:26:50 -0300

    Fix register usage in BranchUnit and clean up assignments

--- design/BranchUnit.sv
+++ design/BranchUnit.sv
@@ -22,16 +22,13 @@ module BranchUnit #(
 
   assign PC_Full = {23'b0, Cur_PC};
 
-  assign PC_Imm = (Jump_Sel) ? Imm + Reg : PC_Full + Imm;
+  assign PC_Imm = (Jump_Sel) ? Imm + Reg1 : PC_Full + Imm;
   assign PC_Four = PC_Full + 32'b100;
   assign Branch_Sel = Branch && AluResult[0];  // 0:Branch is taken; 1:Branch is not taken
 
-  assign BrPC = (Branch_Sel) ? PC_Imm : 32'b0;  // Branch -> PC+Imm   // Otherwise, BrPC value is not important
-  assign PcSel = Branch_Sel;  // 1:branch is taken; 0:branch is not taken(choose pc+4)
-
   assign Result_Sel = Jump_Sel || Branch_Sel; //realiza salto ou branch
 
-  assign BrPC = (Result_Sel) ? PC_Imm : 32'b0;  //se o salto ocorrer o BrPc recebe o imediato // Senão, BrPC recebe  0
-  assign PcSel = Result_Sel;  // 1:desvio tomado; 0:desvio não tomado(próxima instrução pc+4)
+  assign BrPC = (Result_Sel) ? PC_Imm : 32'b0;  //se o salto ocorrer o BrPc recebe o imediato // SenÃ£o, BrPC recebe  0
+  assign PcSel = Result_Sel;  // 1:desvio tomado; 0:desvio nÃ£o tomado(prÃ³xima instruÃ§Ã£o pc+4)
 
 endmodule
