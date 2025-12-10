c8087f8312525b7701dda4660db7ab4db5ed6237
Clara Dias Guimarães <cdg@cin.ufpe.br>
2025-12-10 13:06:36 -0300

    Implement halt signal in Controller module
    Added halt signal to Controller module to manage execution flow.

--- design/Controller.sv
+++ design/Controller.sv
@@ -1,5 +1,6 @@
 `timescale 1ns / 1ps
 
+
 module Controller (
     //Input
     input logic [6:0] Opcode,
@@ -7,24 +8,26 @@ module Controller (
 
     //Outputs
     output logic ALUSrc,
-    //0: The second ALU operand comes from the second register file output (Read data 2); 
+    //0: The second ALU operand comes from the second register file output (Read data 2);
     //1: The second ALU operand is the sign-extended, lower 16 bits of the instruction.
     output logic MemtoReg,
     //0: The value fed to the register Write data input comes from the ALU.
     //1: The value fed to the register Write data input comes from the data memory.
-    output logic RegWrite, //The register on the Write register input is written with the value on the Write data input 
-    output logic MemRead,  //Data memory contents designated by the address input are put on the Read data output
+    output logic RegWrite, //The register on the Write register input is written with the value on the Write data input
+    output logic MemRead, //Data memory contents designated by the address input are put on the Read data output
     output logic MemWrite, //Data memory contents designated by the address input are replaced by the value on the Write data input.
-    output logic [1:0] ALUOp,  //00: LW/SW; 01:Branch; 10: Rtype
-    output logic Branch  //0: branch is not taken; 1: branch is taken
+    output logic [1:0] ALUOp, //00: LW/SW; 01:Branch; 10: Rtype
+    output logic Branch, //0: branch is not taken; 1: branch is taken
+    output logic halt 
 );
 
     logic [6:0] R_TYPE, LW, SW, BR, I_TYPE, J, JR;
+    localparam HALT_OP = 7'b1111111; // opcode do HALT
 
-  assign R_TYPE = 7'b0110011;  //add, and
-  assign LW = 7'b0000011;  //lw
-  assign SW = 7'b0100011;  //sw
-  assign BR = 7'b1100011;  //beq, bne, blt, bge
+  assign R_TYPE = 7'b0110011; //add, and
+  assign LW = 7'b0000011; //lw
+  assign SW = 7'b0100011; //sw
+  assign BR = 7'b1100011; //beq, bne, blt, bge
   assign I_TYPE = 7'b0010011; // addi, slti, slli, srli, srai
   assign J = 7'b1101111; // jal
   assign JR = 7'b1100111; // jalr
@@ -37,5 +40,7 @@ module Controller (
   assign ALUOp[0] = (Opcode == BR || Opcode == J || Opcode == JR);
   assign ALUOp[1] = (Opcode == R_TYPE || Opcode == I_TYPE || Opcode == J || Opcode == JR);
   assign Branch = (Opcode == BR || Opcode == J || Opcode == JR);
-  
+  assign halt = (Opcode == HALT_OP);
+
+
 endmodule
